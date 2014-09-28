ce5cb681a7fc66c343c4296f4f8723d8349a951e
Joh Dokler <joh.dokler@gmail.com>
2014-09-28 21:25:10 +0200

    Config

--- ebisc/settings/base.py
+++ ebisc/settings/base.py
@@ -70,8 +70,10 @@ USE_L10N = True
 
 USE_TZ = True
 
-
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/dev/howto/static-files/
+# -----------------------------------------------------------------------------
+# Static files and media
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, '../var/static/')
+
+# -----------------------------------------------------------------------------
