255f81833b9652e503ed68a71df64a3f41606d65
Joh Dokler <joh.dokler@gmail.com>
2015-04-23 19:58:46 +0200

    Redirect to login

--- ebisc/search/views.py
+++ ebisc/search/views.py
@@ -5,13 +5,15 @@ from ebisc.site.views import render
 from ebisc.celllines.models import Cellline
 
 
-@permission_required('site.can_view_cell_lines', raise_exception=True)
+# @permission_required('site.can_view_cell_lines', raise_exception=True)
+@permission_required('site.can_view_cell_lines')
 def search(request):
 
     return render(request, 'search/search.html', {})
 
 
-@permission_required('site.can_view_cell_lines', raise_exception=True)
+# @permission_required('site.can_view_cell_lines', raise_exception=True)
+@permission_required('site.can_view_cell_lines')
 def cellline(request, biosamples_id):
 
     return render(request, 'search/cellline.html', {
