60dee4e7d92bd753b91808605f409e1969689245
Joh Dokler <joh.dokler@gmail.com>
2015-07-29 14:30:00 +0200

    Remove grapelli admin

--- ebisc/celllines/admin.py
+++ ebisc/celllines/admin.py
@@ -3,6 +3,26 @@ from django.contrib import admin
 from .models import *
 
 
+# -----------------------------------------------------------------------------
+# Helpers
+
+class StackedInline(admin.StackedInline):
+    extra = 0
+
+
+class TabularInline(admin.TabularInline):
+    extra = 0
+
+
+class OneToOneStackedInline(admin.StackedInline):
+    min_num = 1
+    max_num = 1
+    extra = 0
+
+
+# -----------------------------------------------------------------------------
+# Directories
+
 class GenderAdmin(admin.ModelAdmin):
     pass
 
@@ -21,81 +41,48 @@ class ApprovedUseAdmin(admin.ModelAdmin):
 admin.site.register(ApprovedUse, ApprovedUseAdmin)
 
 
-class CellLineCharacterizationInline(admin.StackedInline):
+# -----------------------------------------------------------------------------
+# CellLine
+
+class CellLineCharacterizationInline(OneToOneStackedInline):
     model = CellLineCharacterization
-    min_num = 1
-    max_num = 1
-    extra = 0
-    inline_classes = ('grp-collapse grp-open',)
 
 
-class CelllinechecklistInline(admin.StackedInline):
+class CelllinechecklistInline(OneToOneStackedInline):
     model = Celllinechecklist
-    min_num = 1
-    max_num = 1
-    extra = 0
-    inline_classes = ('grp-collapse grp-open',)
 
 
-class CelllinecultureconditionsInline(admin.StackedInline):
+class CelllinecultureconditionsInline(OneToOneStackedInline):
     model = Celllinecultureconditions
-    min_num = 1
-    max_num = 1
-    extra = 0
-    inline_classes = ('grp-collapse grp-open',)
 
 
-class CelllinederivationInline(admin.StackedInline):
+class CelllinederivationInline(OneToOneStackedInline):
     model = Celllinederivation
-    min_num = 1
-    max_num = 1
-    extra = 0
-    inline_classes = ('grp-collapse grp-open',)
 
 
-class CelllinelabInline(admin.StackedInline):
+class CelllinelabInline(OneToOneStackedInline):
     model = Celllinelab
-    min_num = 1
-    max_num = 1
-    extra = 0
-    inline_classes = ('grp-collapse grp-open',)
 
 
-class CellLineLegalInline(admin.StackedInline):
+class CellLineLegalInline(OneToOneStackedInline):
     model = CellLineLegal
-    min_num = 1
-    max_num = 1
-    extra = 0
-    inline_classes = ('grp-collapse grp-open',)
 
 
-class CelllinevalueInline(admin.StackedInline):
+class CelllinevalueInline(OneToOneStackedInline):
     model = Celllinevalue
-    min_num = 1
-    max_num = 1
-    extra = 0
-    inline_classes = ('grp-collapse grp-open',)
 
 
-class CellLineIntegratingVectorInline(admin.StackedInline):
+class CellLineIntegratingVectorInline(OneToOneStackedInline):
     model = CellLineIntegratingVector
-    min_num = 1
-    max_num = 1
-    extra = 0
-    inline_classes = ('grp-collapse grp-open',)
 
 
-class CellLineNonIntegratingVectorInline(admin.StackedInline):
+class CellLineNonIntegratingVectorInline(OneToOneStackedInline):
     model = CellLineNonIntegratingVector
-    min_num = 1
-    max_num = 1
-    extra = 0
-    inline_classes = ('grp-collapse grp-open',)
 
 
 class CelllineAdmin(admin.ModelAdmin):
 
-    list_display = ['biosamplesid', 'celllinename', 'donor', 'celllineprimarydisease', 'celllinestatus', 'celllinecelltype', 'celllinecollection', 'celllinetissuesource', 'celllinetissuetreatment', 'celllinetissuedate', 'celllinenamesynonyms']
+    list_display = ['biosamplesid', 'celllinename', 'celllinestatus', 'celllinenamesynonyms']
     inlines = (
         CellLineCharacterizationInline,
         CelllinechecklistInline,
@@ -108,378 +95,26 @@ class CelllineAdmin(admin.ModelAdmin):
         CellLineNonIntegratingVectorInline,
     )
 
-    list_filter = ('celllinestatus', 'celllineprimarydisease', 'celllinetissuesource', 'celllinecelltype')
+    list_filter = ('celllinestatus',)
 
 admin.site.register(Cellline, CelllineAdmin)
 
 
-class CelllinecollectionAdmin(admin.ModelAdmin):
-    pass
-
-admin.site.register(Celllinecollection, CelllinecollectionAdmin)
-
-
-class CellLineCultureMediumSupplementAdmin(admin.ModelAdmin):
-    pass
-
-admin.site.register(CellLineCultureMediumSupplement, CellLineCultureMediumSupplementAdmin)
-
-
-class CelllinegenemutationsAdmin(admin.ModelAdmin):
-    pass
-
-admin.site.register(Celllinegenemutations, CelllinegenemutationsAdmin)
-
-
-class CelllinegeneticmodAdmin(admin.ModelAdmin):
-    pass
-
-admin.site.register(Celllinegeneticmod, CelllinegeneticmodAdmin)
-
-
-class CelllinegenomeseqAdmin(admin.ModelAdmin):
-    pass
-
-admin.site.register(Celllinegenomeseq, CelllinegenomeseqAdmin)
-
-
-class CelllinegenotypingotherAdmin(admin.ModelAdmin):
-    pass
-
-admin.site.register(Celllinegenotypingother, CelllinegenotypingotherAdmin)
-
-
-class CelllinehlatypingAdmin(admin.ModelAdmin):
-    pass
-
-admin.site.register(Celllinehlatyping, CelllinehlatypingAdmin)
-
-
-class CellLineKaryotypeAdmin(admin.ModelAdmin):
-    pass
-
-admin.site.register(CellLineKaryotype, CellLineKaryotypeAdmin)
-
-
-class CelllineorganizationAdmin(admin.ModelAdmin):
-
-    list_display = ['orgcellline', 'organization', 'celllineorgtype', 'orgstatus', 'orgregistrationdate']
-
-admin.site.register(Celllineorganization, CelllineorganizationAdmin)
-
-
-class CelllineorgtypeAdmin(admin.ModelAdmin):
-    pass
-
-admin.site.register(Celllineorgtype, CelllineorgtypeAdmin)
-
-
-class CellLinePublicationAdmin(admin.ModelAdmin):
-    list_display = ['cell_line', 'reference_type', 'reference_title', 'reference_url']
-    list_display_links = ['cell_line']
-    list_filter = ['reference_type']
-
-admin.site.register(CellLinePublication, CellLinePublicationAdmin)
-
-
-class CelllinesnpAdmin(admin.ModelAdmin):
-    pass
-
-admin.site.register(Celllinesnp, CelllinesnpAdmin)
-
-
-class CelllinesnpdetailsAdmin(admin.ModelAdmin):
-    pass
-
-admin.site.register(Celllinesnpdetails, CelllinesnpdetailsAdmin)
-
-
-class CelllinesnprslinksAdmin(admin.ModelAdmin):
-    pass
-
-admin.site.register(Celllinesnprslinks, CelllinesnprslinksAdmin)
-
-
-class CelllinestatusAdmin(admin.ModelAdmin):
-    pass
-
-admin.site.register(Celllinestatus, CelllinestatusAdmin)
-
-
-class CelllinestrfingerprintingAdmin(admin.ModelAdmin):
-    pass
-
-admin.site.register(Celllinestrfingerprinting, CelllinestrfingerprintingAdmin)
-
-
-class CelltypeAdmin(admin.ModelAdmin):
-    pass
-
-admin.site.register(Celltype, CelltypeAdmin)
-
-
-class Clinicaltreatmentb4donationAdmin(admin.ModelAdmin):
-    pass
-
-admin.site.register(Clinicaltreatmentb4donation, Clinicaltreatmentb4donationAdmin)
-
-
-class ContactAdmin(admin.ModelAdmin):
-    pass
-
-admin.site.register(Contact, ContactAdmin)
-
-
-class ContacttypeAdmin(admin.ModelAdmin):
-    pass
-
-admin.site.register(Contacttype, ContacttypeAdmin)
-
-
-class CountryAdmin(admin.ModelAdmin):
-    pass
-
-admin.site.register(Country, CountryAdmin)
-
-
-class CultureMediumAdmin(admin.ModelAdmin):
-    pass
-
-admin.site.register(CultureMedium, CultureMediumAdmin)
-
-
-class CultureMediumOtherAdmin(admin.ModelAdmin):
-    pass
-
-admin.site.register(CultureMediumOther, CultureMediumOtherAdmin)
-
-
-class CulturesystemAdmin(admin.ModelAdmin):
-    pass
-
-admin.site.register(Culturesystem, CulturesystemAdmin)
-
-
-class DiseaseAdmin(admin.ModelAdmin):
-    pass
-
-admin.site.register(Disease, DiseaseAdmin)
-
-
-class DocumentAdmin(admin.ModelAdmin):
-    pass
-
-admin.site.register(Document, DocumentAdmin)
-
-
-class DocumenttypeAdmin(admin.ModelAdmin):
-    pass
-
-admin.site.register(Documenttype, DocumenttypeAdmin)
-
-
-class DonorAdmin(admin.ModelAdmin):
-
-    list_display = ['biosamplesid', 'gender', 'countryoforigin', 'primarydisease', 'diseaseadditionalinfo', 'provider_donor_ids']
-    list_display_links = ['biosamplesid']
-    list_filter = ['gender', 'countryoforigin']
-
-admin.site.register(Donor, DonorAdmin)
-
-
-class EbisckeywordAdmin(admin.ModelAdmin):
-    pass
-
-admin.site.register(Ebisckeyword, EbisckeywordAdmin)
-
-
-class EnzymaticallyAdmin(admin.ModelAdmin):
-    pass
-
-admin.site.register(Enzymatically, EnzymaticallyAdmin)
-
-
-class EnzymeFreeAdmin(admin.ModelAdmin):
-    pass
-
-admin.site.register(EnzymeFree, EnzymeFreeAdmin)
-
-
-class GermlayerAdmin(admin.ModelAdmin):
-    pass
-
-admin.site.register(Germlayer, GermlayerAdmin)
-
-
-class HlaAdmin(admin.ModelAdmin):
-    pass
-
-admin.site.register(Hla, HlaAdmin)
-
-
-class KaryotypeMethodAdmin(admin.ModelAdmin):
-    pass
-
-admin.site.register(KaryotypeMethod, KaryotypeMethodAdmin)
-
-
-class KeywordAdmin(admin.ModelAdmin):
-    pass
-
-admin.site.register(Keyword, KeywordAdmin)
-
-
-class MoleculeReferenceInline(admin.StackedInline):
-    model = MoleculeReference
-    extra = 0
-
-
-class MoleculeAdmin(admin.ModelAdmin):
-    list_display = ['name', 'kind']
-    list_filter = ['kind']
-    inlines = [MoleculeReferenceInline]
-
-admin.site.register(Molecule, MoleculeAdmin)
-
-
-class MorphologymethodAdmin(admin.ModelAdmin):
-    pass
-
-admin.site.register(Morphologymethod, MorphologymethodAdmin)
-
-
-class OrganizationAdmin(admin.ModelAdmin):
-    pass
-
-admin.site.register(Organization, OrganizationAdmin)
-
-
-class OrgtypeAdmin(admin.ModelAdmin):
-    pass
-
-admin.site.register(Orgtype, OrgtypeAdmin)
-
-
-class PassageMethodAdmin(admin.ModelAdmin):
-    pass
-
-admin.site.register(PassageMethod, PassageMethodAdmin)
-
-
-class PersonAdmin(admin.ModelAdmin):
-    pass
-
-admin.site.register(Person, PersonAdmin)
-
-
-class PhenotypeAdmin(admin.ModelAdmin):
-    pass
-
-admin.site.register(Phenotype, PhenotypeAdmin)
-
-
-class PhonecountrycodeAdmin(admin.ModelAdmin):
-    pass
-
-admin.site.register(Phonecountrycode, PhonecountrycodeAdmin)
-
-
-class PostcodeAdmin(admin.ModelAdmin):
-    pass
-
-admin.site.register(Postcode, PostcodeAdmin)
-
-
-class PrimaryCellDevelopmentalStageAdmin(admin.ModelAdmin):
-    pass
-
-admin.site.register(PrimaryCellDevelopmentalStage, PrimaryCellDevelopmentalStageAdmin)
-
-
-class ProteinSourceAdmin(admin.ModelAdmin):
-    pass
-
-admin.site.register(ProteinSource, ProteinSourceAdmin)
-
-
-class PublisherAdmin(admin.ModelAdmin):
-    pass
-
-admin.site.register(Publisher, PublisherAdmin)
-
-
-class Reprogrammingmethod1Admin(admin.ModelAdmin):
-    pass
-
-admin.site.register(Reprogrammingmethod1, Reprogrammingmethod1Admin)
-
-
-class Reprogrammingmethod2Admin(admin.ModelAdmin):
-    pass
-
-admin.site.register(Reprogrammingmethod2, Reprogrammingmethod2Admin)
-
-
-class Reprogrammingmethod3Admin(admin.ModelAdmin):
-    pass
-
-admin.site.register(Reprogrammingmethod3, Reprogrammingmethod3Admin)
-
-
-class StrfplocusAdmin(admin.ModelAdmin):
-    pass
-
-admin.site.register(Strfplocus, StrfplocusAdmin)
-
-
-class SurfaceCoatingAdmin(admin.ModelAdmin):
-    pass
-
-admin.site.register(SurfaceCoating, SurfaceCoatingAdmin)
-
-
-class TissuesourceAdmin(admin.ModelAdmin):
-    pass
-
-admin.site.register(Tissuesource, TissuesourceAdmin)
-
-
-class UnitAdmin(admin.ModelAdmin):
-    pass
-
-admin.site.register(Unit, UnitAdmin)
-
-
-class VectorfreereprogramfactorAdmin(admin.ModelAdmin):
-    pass
-
-admin.site.register(Vectorfreereprogramfactor, VectorfreereprogramfactorAdmin)
-
-
 # -----------------------------------------------------------------------------
-# Cell line vector
+# Batches
 
-class NonIntegratingVectorAdmin(admin.ModelAdmin):
-    pass
-
-admin.site.register(NonIntegratingVector, NonIntegratingVectorAdmin)
+class BatchAliquotInline(TabularInline):
+    model = CelllineAliquot
 
 
-class IntegratingVectorAdmin(admin.ModelAdmin):
-    pass
+class CelllineBatchAdmin(admin.ModelAdmin):
 
-admin.site.register(IntegratingVector, IntegratingVectorAdmin)
+    list_display = ['biosamplesid', 'batch_id', 'cell_line']
+    inlines = (BatchAliquotInline,)
 
+    # list_filter = ('celllinestatus',)
 
-class VirusAdmin(admin.ModelAdmin):
-    pass
+admin.site.register(CelllineBatch, CelllineBatchAdmin)
 
-admin.site.register(Virus, VirusAdmin)
-
-
-class TransposonAdmin(admin.ModelAdmin):
-    pass
-
-admin.site.register(Transposon, TransposonAdmin)
 
 # -----------------------------------------------------------------------------
