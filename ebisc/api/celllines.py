390adb7ea13fabdec7707ed211030eab57e2d2e8
Maja Brajnik <maja.brajnik@gmail.com>
2015-09-21 21:04:10 +0200

    Include batch data in the API

--- ebisc/api/celllines.py
+++ ebisc/api/celllines.py
@@ -7,7 +7,7 @@ from tastypie.authorization import ReadOnlyAuthorization
 from tastypie import fields
 
 from . import IndentedJSONSerializer
-from ..celllines.models import Donor, Disease, Cellline, CelllineCultureConditions, CelllineDerivation, CellLineKaryotype, Organization
+from ..celllines.models import Donor, Disease, Cellline, CelllineCultureConditions, CelllineDerivation, CellLineKaryotype, Organization, CelllineBatch, CelllineBatchImages, BatchCultureConditions
 
 
 # -----------------------------------------------------------------------------
@@ -138,6 +138,87 @@ class OrganizationResource(ModelResource):
 
 
 # -----------------------------------------------------------------------------
+# Batch Images
+
+class CelllineBatchImagesResource(ModelResource):
+    image_file = fields.FileField('image_file')
+    image_md5 = fields.CharField('image_md5')
+    magnification = fields.CharField('magnification', null=True)
+    time_point = fields.CharField('time_point', null=True)
+
+    class Meta:
+        queryset = CelllineBatchImages.objects.all()
+        include_resource_uri = False
+        fields = ('image_file', 'image_md5', 'magnification', 'time_point')
+
+
+# -----------------------------------------------------------------------------
+# Batch Culture conditions
+
+class BatchCultureConditionsResource(ModelResource):
+    culture_medium = fields.CharField('culture_medium', null=True)
+    passage_method = fields.CharField('passage_method', null=True)
+    matrix = fields.CharField('matrix', null=True)
+    o2_concentration = fields.CharField('o2_concentration', null=True)
+    co2_concentration = fields.CharField('co2_concentration', null=True)
+    temperature = fields.CharField('temperature', null=True)
+
+    class Meta:
+        queryset = BatchCultureConditions.objects.all()
+        include_resource_uri = False
+        fields = ('culture_medium')
+
+
+# -----------------------------------------------------------------------------
+# Batch
+
+class CelllineBatchResource(ModelResource):
+
+    biosamples_id = fields.CharField('biosamples_id', unique=True)
+    batch_id = fields.CharField('batch_id')
+
+    vials_at_roslin = fields.IntegerField('vials_at_roslin', null=True)
+    vials_shipped_to_ecacc = fields.IntegerField('vials_shipped_to_ecacc', null=True)
+    vials_shipped_to_fraunhoffer = fields.IntegerField('vials_shipped_to_fraunhoffer', null=True)
+
+    culture_conditions = fields.ToOneField(BatchCultureConditionsResource, 'batchcultureconditions', null=True, full=True)
+
+    certificate_of_analysis = fields.DictField(null=True)
+
+    images = fields.ToManyField(CelllineBatchImagesResource, 'images', null=True, full=True)
+
+    class Meta:
+        queryset = CelllineBatch.objects.all()
+        resource_name = 'batches'
+        list_allowed_methods = ['get']
+        detail_allowed_methods = ['get']
+
+        detail_uri_name = 'biosamples_id'
+
+        authentication = ApiKeyAuthentication()
+        authorization = ReadOnlyAuthorization()
+
+        serializer = IndentedJSONSerializer()
+
+        fields = ('biosamples_id', 'batch_id')
+
+    def get_schema(self, request, **kwargs):
+        raise Http404
+
+    def dehydrate_certificate_of_analysis(self, bundle):
+
+        if not bundle.obj.certificate_of_analysis:
+            return None
+        else:
+            res = {
+                'file': bundle.obj.certificate_of_analysis,
+                'md5': bundle.obj.certificate_of_analysis_md5,
+            }
+
+            return res
+
+
+# -----------------------------------------------------------------------------
 # Cellline
 
 class CelllineResource(ModelResource):
@@ -159,11 +240,11 @@ class CelllineResource(ModelResource):
     ecacc_cat_no = fields.CharField('ecacc_id', unique=True, null=True)
 
     name = fields.CharField('name', unique=True)
-    alternative_names = fields.CharField('alternative_names')
+    alternative_names = fields.CharField('alternative_names', null=True)
 
     primary_disease = fields.ToOneField(DiseaseResource, 'primary_disease', null=True, full=True)
     primary_cell_type = fields.ToOneField(CelllineDerivationResource, 'derivation', null=True, full=True)
-    culture_conditions = fields.ToOneField(CelllineCultureConditionsResource, 'celllinecultureconditions', full=True)
+    # culture_conditions = fields.ToOneField(CelllineCultureConditionsResource, 'celllinecultureconditions', full=True)
     cellline_karyotype = fields.ToOneField(CellLineKaryotypeResource, 'karyotype', null=True, full=True)
 
     donor_age = fields.CharField('donor_age', null=True)
@@ -172,6 +253,8 @@ class CelllineResource(ModelResource):
     depositor = fields.ToOneField(OrganizationResource, 'generator', null=True, full=True)
 
     reprogramming_method = fields.DictField(null=True)
+
+    batches = fields.ToManyField(CelllineBatchResource, 'batches', null=True, full=True)
 
     class Meta:
         queryset = Cellline.objects.all()
@@ -192,7 +275,7 @@ class CelllineResource(ModelResource):
     def get_schema(self, request, **kwargs):
         raise Http404
 
-    def dehydrate_alternate_names(self, bundle):
+    def dehydrate_alternative_names(self, bundle):
         return value_list_of_string(bundle.obj.alternative_names)
 
     def dehydrate_reprogramming_method(self, bundle):
--- ebisc/api/urls.py
+++ ebisc/api/urls.py
@@ -2,10 +2,11 @@ from tastypie.api import Api
 
 from django.conf.urls import patterns, include, url
 
-from .celllines import CelllineResource
+from .celllines import CelllineResource, CelllineBatchResource
 
 api = Api(api_name='v0')
 api.register(CelllineResource())
+api.register(CelllineBatchResource())
 
 urlpatterns = patterns('',
     url(r'^', include(api.urls)),
