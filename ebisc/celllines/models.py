f1c84ac5526d760b48a32d210ba640f5566a4bbd
Maja Brajnik <maja.brajnik@gmail.com>
2015-10-01 16:35:04 +0200

    Add disease diagnosis to cell line

--- ebisc/celllines/models.py
+++ ebisc/celllines/models.py
@@ -187,6 +187,7 @@ class Cellline(models.Model):
     derivation_country = models.ForeignKey('Country', verbose_name=_(u'Derivation country'), null=True, blank=True)
 
     primary_disease = models.ForeignKey('Disease', verbose_name=_(u'Diagnosed disease'), null=True, blank=True)
+    primary_disease_diagnosis = models.CharField(_(u'Disease diagnosis'), max_length=12, null=True, blank=True)
     primary_disease_stage = models.CharField(_(u'Disease stage'), max_length=100, null=True, blank=True)
     disease_associated_phenotypes = ArrayField(models.CharField(max_length=500), verbose_name=_(u'Disease associated phenotypes'), null=True)
     affected_status = models.CharField(_(u'Affected status'), max_length=12, null=True, blank=True)
