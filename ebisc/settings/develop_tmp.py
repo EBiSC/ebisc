5758e765cba6f24fc792ceded75d5d436399002e
Maja Brajnik <maja.brajnik@gmail.com>
2015-11-20 10:22:54 +0100

    Tmp server settings

---
+++ ebisc/settings/develop_tmp.py
@@ -0,0 +1,25 @@
+from .base import *
+
+DEBUG = False
+TEMPLATE_DEBUG = False
+
+ALLOWED_HOSTS = ['ebisc.douglasconnect.com']
+
+SERVER_EMAIL = 'EBiSC Staging <joh@douglasconnect.com>'
+
+# -----------------------------------------------------------------------------
+# Database
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'ebisc',
+        'USER': 'www',
+        'HOST': 'db',
+    }
+}
+
+ELASTIC_INDEX = 'ebisc'
+ELASTIC_HOSTS = [{'host': 'elastic', 'port': 9200}]
+
+# -----------------------------------------------------------------------------
--- fabfile.py
+++ fabfile.py
@@ -15,7 +15,7 @@ env.shell = '/bin/sh -c'
 env.activate = '. %s/var/virtualenv/bin/activate' % DESTDIR
 
 env.roledefs = {
-    'develop': ['www@ebisc.douglasconnect.com'],
+    'develop': ['www@uwsgi'],
     'staging': ['www@ebisc-stage.douglasconnect.com'],
     'production': ['www@ebisc-prod.douglasconnect.com'],
 }
