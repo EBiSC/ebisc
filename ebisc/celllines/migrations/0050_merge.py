f11f659de8d886e10d746af4a78f9b120eb962bd
Maja Brajnik <maja.brajnik@gmail.com>
2016-10-05 10:56:00 +0200

    Merge migrations because of conflicts

---
+++ ebisc/celllines/migrations/0050_merge.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('celllines', '0048_auto_20160928_2153'),
+        ('celllines', '0049_cellline_comparator_cell_line_relation'),
+    ]
+
+    operations = [
+    ]
