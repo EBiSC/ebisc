42b2e901c7d1459fc96fdd55ce60c1b4446f1c05
Johannes Dewender <johannes.dewender@charite.de>
2019-03-20 18:06:13 +0100

    allow .charite.de for develop role

--- ebisc/settings/develop.py
+++ ebisc/settings/develop.py
@@ -3,6 +3,8 @@ from .base import *
 DEBUG = True
 TEMPLATE_DEBUG = True
 
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', '[::1]', '.charite.de']
+
 INTERNAL_IPS = ['127.0.0.1']
 
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
